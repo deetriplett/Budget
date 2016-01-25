bc02484a6789e31ce26febceaaf4307b4779f058
Dawn Monroe <deetriplett@gmail.com>
2016-01-25 18:57:25 -0500

    Polished and Pretty HTML updated, created folders for files

---
+++ js/budget.js
@@ -0,0 +1,22 @@
+var 1;
+prompt ('Household size?') =
+
+var 2
+prompt ('Monthly Income')
+
+var 3
+prompt ('Little Debt or Big Debt?')
+
+var 4
+prompt ('Frugal or Fancy?')
+
+//Hide results
+$(".results").hide();
+//add button
+$(".results").append("<button>Show me the money!</button>");
+//when button pressed
+$(.button)click(function(){
+  $("p").show();
+  $(this).remove();
+}); //show results
+  //get rid of button
