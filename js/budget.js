53f940b0e0f6df58e6fa2266fdc48d7bae50ed96
Dawn Monroe <deetriplett@gmail.com>
2016-01-30 10:55:43 -0500

    Modified contact page, added script tag in index, and fleshed out JS a little.

--- js/budget.js
+++ js/budget.js
@@ -1,14 +1,38 @@
-var 1;
-prompt ('Household size?') =
 
-var 2
-prompt ('Monthly Income')
+var income = prompt('Household Monthly Income?');
+if ( income === ) {
+    document.write('HOUSING:' + income*.24');
+} else {
+  document.write('HOUSING:' + income*.24');}
 
-var 3
-prompt ('Little Debt or Big Debt?')
+var household = prompt("How many people dwell in said household?");
 
-var 4
-prompt ('Frugal or Fancy?')
+var debt = prompt('Little debt or lots of debt?');//add check boxes, little or lots
+if ( debt.toUpperCasease === LITTLE) {
+  document.write('<p>SAVINGS: ' + income*.10 </p>'); }
+ else { document.write('<p>DEBTS: ' + income*.05</p>');
+}
+if (debt.toUpperCasease === LOTS) {
+  document.write('<p>SAVINGS: ' + income*.05</p>');}
+ else { document.write('<p>DEBTS: ' + income*.10</p>');
+}
+
+var frugal = prompt('Are you frugal or fancy?'); //add check boxes, frugal or fancy
+if (frugal.toUpperCase === FRUGAL) {
+  document.write ('<p>Fun:' + income*.05 </p>');}
+    else { document.write('<p>Fun: + income*.09 </p>');
+}
+
+//poor & rich, poor w/ kids, (flip savings/debts and fun/other)
+//document.write('HOUSING: 25%/ 24');
+//document.write('UTILITIES:10/ 10 ');
+//document.write('FOOD:14/  15');
+//document.write('CAR:12/  10');
+//document.write('HEALTH:6/ 9 ');
+//document.write('FUN:7/  6');
+//document.write('SAVINGS:10/ 10 ');
+//document.write('DEBTS:6/ 5 ');
+//document.write('OTHER:10/ 11 ')
 
 //Hide results
 $(".results").hide();
